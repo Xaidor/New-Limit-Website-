e323bcd54e3fb6f566ca1c661ecea0d35503ff09
Kaylen Anderson <129983434+Xaidor@users.noreply.github.com>
2024-12-28 12:04:54 -0500

    edited impirt path

--- website/app.jsx
+++ website/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react' ;
 import ReactDOM from 'react-dom/client';
-import './app.css';
+import './website/app.css';
 
 //create a new logo for the homepage with the companys name later
 function HomePage() {
